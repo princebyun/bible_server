f5c62661ef2af347c59202a71e85b3b237c772b8
princebyun <hkbyun@plani.co.kr>
2025-12-18 10:06:35 +0900

    feat : ai api 모델 변경

--- src/main/java/bible/bible/service/CleanBotService.java
+++ src/main/java/bible/bible/service/CleanBotService.java
@@ -29,13 +29,11 @@ public class CleanBotService {
         String prompt = "너는 게시판 관리자야. 다음 텍스트를 분석해.\n" +
                 "텍스트: \"" + userText + "\"\n" +
                 "공격적이거나 비속어가 있는지 판단해서 무조건 다음 JSON 형식으로만 답해(마크다운 backtick 없이):\n" +
-                "{ \"isSafe\": boolean, \"reason\": string }\n" +
-                "reason은 한국어로만 대답해." +
-                "안전하다면 reason은 '건전한 게시글입니다'라고 써.";
+                "{ \"isSafe\": boolean, \"reason\": string }\n";
 
         try {
             Message message = new Message("user", prompt);
-            OllamaRequest request = new OllamaRequest("llama3.2", Collections.singletonList(message));
+            OllamaRequest request = new OllamaRequest("qwen2.5:3b", Collections.singletonList(message));
 
             String responseString = restTemplate.postForObject(url, request, String.class);
             JsonNode root = objectMapper.readTree(responseString);
@@ -44,7 +42,7 @@ public class CleanBotService {
             return objectMapper.readValue(content, CleanBotResult.class);
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("Llama API 호출 실패: " + e.getMessage());
+            throw new RuntimeException("qwen2 API 호출 실패: " + e.getMessage());
         }
     }
 
@@ -55,13 +53,13 @@ public class CleanBotService {
         try {
             String base64Image = Base64.getEncoder().encodeToString(file.getBytes());
             String promptText = "이 이미지가 게시판에 올리기에 안전한지 분석해.\n" +
-                    "선정적이거나 폭력적이거나 혐오스러운 요소가 있는지 확인해.\n" +
+                    "nudity, naked,blood, violence, sexual, weapon같은 요소들이 있는지 판단해서 안전한지 결과를 알려줘.\n" +
                     "결과는 무조건 JSON { \"isSafe\": boolean, \"reason\": string } 으로 답해.\n" +
                     "reason은 한국어로만 대답해.";
 
             Message message = new Message("user", promptText, Collections.singletonList(base64Image));
 
-            OllamaRequest request = new OllamaRequest("llava-phi3", Collections.singletonList(message));
+            OllamaRequest request = new OllamaRequest("moondream", Collections.singletonList(message));
 
             String responseString = restTemplate.postForObject(url, request, String.class);
             JsonNode root = objectMapper.readTree(responseString);
@@ -71,7 +69,7 @@ public class CleanBotService {
 
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("llava-phi3 이미지 분석 실패: " + e.getMessage());
+            throw new RuntimeException("moondream 이미지 분석 실패: " + e.getMessage());
         }
     }
 }
